c3751f10622dd675f250fb1d39cb3b9f9c20069d
sscs12345 <shauryashah98@gmail.com>
2021-07-15 17:33:18 -0700

    Create npc.sh

---
+++ npc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wget http://media.steampowered.com/client/steamcmd_linux.tar.gz
+tar -xzf steamcmd_linux.tar.gz
+chmod 777 steamcmd.sh
+chmod -R 777 linux32
+./steamcmd.sh +login anonymous +force_install_dir game +app_set_config 90 mod czero +app_update 90 validate +exit
+rm -rf steamcmd_linux.tar.gz
+rm -rf steamcmd.sh
+rm -rf steam.sh
+rm -rf linux32
